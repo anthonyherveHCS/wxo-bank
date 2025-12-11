e4b0bb51fe3feecd21e7246388ca03506970dce9
anthonyherveHCS <herve.a@fr.ibm.com>
2025-12-11 13:27:33 +0100

    Change double quotes to single quotes in configuration

--- myscript.js
+++ myscript.js
@@ -1,17 +1,16 @@
 
 window.wxOConfiguration = {
     orchestrationID: 
-"f665a69257a9fbe8b8bf0f77bcee465e_cb18e62f-3b09-4337-a728-3693e9a8f16f",
-    hostURL: "https://us-south.watson-orchestrate.cloud.ibm.com",
+'f665a69257a9fbe8b8bf0f77bcee465e_cb18e62f-3b09-4337-a728-3693e9a8f16f',
+    hostURL: 'https://us-south.watson-orchestrate.cloud.ibm.com',
     rootElementID: "root",
     showLauncher: true,
     crn: 
-"crn:v1:bluemix:public:watsonx-orchestrate:us-south:a/f665a69257a9fbe8b8bf0f77bcee465e:cb18e62f-3b09-4337-a
-728-3693e9a8f16f::",
-    deploymentPlatform: "ibmcloud",
+'crn:v1:bluemix:public:watsonx-orchestrate:us-south:a/f665a69257a9fbe8b8bf0f77bcee465e:cb18e62f-3b09-4337-a728-3693e9a8f16f::',
+    deploymentPlatform: 'ibmcloud',
     chatOptions: {
-      agentId: "b753ff4e-e695-4a83-b1d7-20732c48b6ca",
-      agentEnvironmentId: "3075c23e-744b-426a-9b64-fea574ce34fb"
+      agentId: 'b753ff4e-e695-4a83-b1d7-20732c48b6ca',
+      agentEnvironmentId: '3075c23e-744b-426a-9b64-fea574ce34fb'
   }
 };
 
